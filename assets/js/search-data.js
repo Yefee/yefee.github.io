691f201f3d57ee77f7b106dcdd80feae63d4e005
Yefee <yefee90@gmail.com>
2025-10-18 16:52:30 +0000

    Deploying to gh-pages from @ Yefee/yefee.github.io@88bd50bdd26199010b3f70eda4ce03a63462d893 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -52,8 +52,8 @@ ninja.data = [{
             window.location.href = "/teaching/";
           },
         },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
+          title: "People",
+          description: "Members of Climate Dynamics Lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
